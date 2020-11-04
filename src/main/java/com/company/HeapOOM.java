c709a77babf78195308503b858a2fc32c16f9794
lbh1995 <1044072893@qq.com>
2020-11-04 09:43:25 +0800

    master

--- src/main/java/com/company/HeapOOM.java
+++ src/main/java/com/company/HeapOOM.java
@@ -9,7 +9,7 @@ public class HeapOOM {
     }
 
     public static void main(String[] args) {
-        System.out.println("lbh_dev");
+        System.out.println("lbh_master");
         List<OOMObject> list = new ArrayList<>();
         while (true){
             list.add(new OOMObject());
